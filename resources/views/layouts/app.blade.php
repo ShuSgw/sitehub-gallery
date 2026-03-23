ecafd64cf38f5a843194f192ab9c08e532611267
shuhei <sgw.sgw.shu@gmail.com>
2026-03-24 00:42:38 +0900

    【追加】基本ルートとblade

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    {{-- 各ページごとにタイトルを差し込むもし指定されなければ「sgwのアプリ」がデフォルトになる --}}
+    <title>@yield('title', 'sgwのアプリ')</title>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+
+<body class="bg-slate-50 flex flex-col min-h-screen">
+
+    <header class="bg-black py-5">
+        <div class="max-w-4xl mx-auto px-4">
+            <h1 class="text-lg font-semibold text-white">
+                SGWのサイト置き場
+            </h1>
+        </div>
+    </header>
+
+    <main class="flex-1 max-w-4xl mx-auto px-4 py-12 w-full">
+        {{-- 各ページのメインコンテンツがここに入る --}}
+        @yield('content')
+    </main>
+
+    <footer class="bg-black text-center py-6 w-full">
+        <p class="text-sm text-white">© 2026</p>
+    </footer>
+
+</body>
+
+</html>
