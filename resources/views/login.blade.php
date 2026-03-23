929bfac2be9374857ef7c6d33f9c3209e54b60de
shuhei <sgw.sgw.shu@gmail.com>
2026-03-24 02:13:59 +0900

    【追加】ログイン機能追加

---
+++ resources/views/login.blade.php
@@ -0,0 +1,11 @@
+{{-- layouts/app.blade.php を継承する --}}
+@extends('layouts.app')
+
+{{-- titleセクションに値を渡す（1行書き） --}}
+@section('title', 'ホーム')
+
+{{-- contentセクション（複数行なので endsection を使う） --}}
+@section('content')
+    {{-- ここが実際にページごとに変わる部分 --}}
+    <p>ログイン</p>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,28 @@
 
 use Illuminate\Support\Facades\Route;
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
+use Illuminate\Http\Request;
+// リクエスト（フォーム入力）を扱う
+use Illuminate\Support\Facades\Auth;
+// 認証（ログイン処理）を扱う
 
 Route::get('/', function () {
     return view('home');
+});
+
+Route::get('/login', function () {
+    return view('login');
+});
+
+Route::post('/login', function (Request $request) {
+    // POST /login に来たリクエストを受け取り、$requestとして使える
+    $credentials = $request->only('email', 'password');
+    // 入力からemailとpasswordだけ取得
+
+    if (Auth::attempt($credentials)) {
+        // usersテーブルで認証→成功ならログイン状態にする
+        return redirect('/dashboard'); // 成功時の遷移
+    }
+
+    return back(); // 失敗したら元のページに戻る
 });
